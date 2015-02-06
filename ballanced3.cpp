6293929e3039d727c28039ee7177eef118bb1e17
TomPlano <planot@wit.edu>
2015-02-06 09:59:11 -0500

    added the base3_to_ballanced3 function, line 57

--- ballanced3.cpp
+++ ballanced3.cpp
@@ -19,7 +19,7 @@ int const size=16; // size of your RAM, 16 Trits
 class Trit{
 	public:
 	Trit(int x){
-	int_to_trit(x);
+	int_to_base3(x);
 	}
 	void output(){
 		for (int i=size-1; i>=0; i--){
@@ -40,16 +40,32 @@ class Trit{
     return result;
 }
 	int count=size;
-	int trit_val[size];  //array to keep final answer of conversion 
-	void int_to_trit(int input_val){
-		trit_val[count]=input_val/(ipow(3,count));  // value to be converted/ 3^current place (1's, 3's, 9's, 27's, etc) = base three value for that place
+	int base3[size];  //array to keep final answer of conversion 
+	void int_to_base3(int input_val){
+		base3[count]=input_val/(ipow(3,count));  // value to be converted/ 3^current place (1's, 3's, 9's, 27's, etc) = base three value for that place
 		input_val=input_val%(ipow(3,count)); 	// next input value is the remainder of  the previous line
 		count--;
 		if (count<0){  //if the conversion is finished...
+			base3_to_ballanced3();
 			return;
 		}
 		else{
-			int_to_trit(input_val); // recur into function 
+			int_to_base3(input_val); // recur into function 
+		}
+	}
+	char trit_val[size];
+	void base3_to_ballanced3(){
+		for(int i=0; i<size; i++){
+			if (base3[i]==2){
+				trit_val[i]='+';
+			}
+			else if (base3[i]==1){
+				trit_val[i]='-';
+			}
+			else{
+				trit_val[i]='0';
+			}
+			
 		}
 	}
 };
