6ed2ae3b19da44181dcc35f45894f99daee7f274
TomPlano <planot@wit.edu>
2015-02-06 09:27:38 -0500

    general comments added

--- ballanced3.cpp
+++ ballanced3.cpp
@@ -14,7 +14,7 @@
 #include<iostream>
 using namespace std; 
 
-int const size=16;
+int const size=16; // size of your RAM, 16 Trits 
 
 class Trit{
 	public:
@@ -29,7 +29,7 @@ class Trit{
 	}
 	
 	private:
-	int ipow(int base, int exp){
+	int ipow(int base, int exp){  //googled power function, was having trouble with the cmath library. Needs to be removed 
     int result = 1;
     while (exp){
         if (exp & 1)
@@ -40,21 +40,21 @@ class Trit{
     return result;
 }
 	int count=size;
-	int trit_val[size];
+	int trit_val[size];  //array to keep final answer of conversion 
 	void int_to_trit(int input_val){
-		trit_val[count]=input_val/(ipow(3,count));
-		input_val=input_val%(ipow(3,count));
+		trit_val[count]=input_val/(ipow(3,count));  // value to be converted/ 3^current place (1's, 3's, 9's, 27's, etc) = base three value for that place
+		input_val=input_val%(ipow(3,count)); 	// next input value is the remainder of  the previous line
 		count--;
-		if (count<0){
+		if (count<0){  //if the conversion is finished...
 			return;
 		}
 		else{
-			int_to_trit(input_val);
+			int_to_trit(input_val); // recur into function 
 		}
 	}
 };
 
-int main() {
+int main() { // test program
 	
 	int a;
 	cout<< "enter value to be converted less then 43046721 and greater then 0 (16 trits unsigned)"<<endl;
